7c36efb3983697870891ecbd00cac0546c34be2f
Dexter <dsg841@mail.usask.ca>
2017-10-12 16:08:58 -0600

    removed mark on registration

--- src/registrationTool.java
+++ src/registrationTool.java
@@ -55,11 +55,7 @@ public class registrationTool extends JFrame {
             {
 
                 // JFormDesigner evaluation mark
-                pnlWelcome.setBorder(new javax.swing.border.CompoundBorder(
-                    new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
-                        "JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
-                        javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
-                        java.awt.Color.red), pnlWelcome.getBorder())); pnlWelcome.addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
+              
 
                 pnlWelcome.setLayout(new FlowLayout());
 
