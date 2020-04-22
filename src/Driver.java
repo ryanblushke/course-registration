c882fd3c3d208ac0cabb943e499921097ba38f3f
Ryan Blushke <ryan.blushke@gmail.com>
2020-04-22 13:23:34 -0600

    Updating Driver. RB

--- src/Driver.java
+++ src/Driver.java
@@ -32,9 +32,9 @@ public class Driver {
      * Assigns variables url, username, and password with our GitLab Repository credentials
      */
     public Driver() {
-        url = "jdbc:mysql://db.cs.usask.ca:3306/cmpt370_bigdsm";
-        username = "cmpt370_bigdsm";
-        password = "bnxOEOlO7i8Xlv4FWrJc";
+        url = "jdbc:mysql://example.ca:1234/directory";
+        username = "";
+        password = "";
         T1_Schedule = new Schedule(); // HOLDS TERM 1 SCHEDULE and Schedule from database
         T2_Schedule = new Schedule(); // HOLDS TERM 2 SCHEDULE and Schedule from database
         T1_Schedule_DB = new Schedule(); // HOLDS TERM 1 SCHEDULE JUST FROM DATABASE
